f15376d30c03f4b28e7fc2b4029ef5a5a871366b
sebastian-salinas <sseba67@gmail.com>
2024-07-28 22:28:53 +0200

    Añadimos la primer pestaña, to do list, colocamos la funcion para añadir tareas y poder eliminarlas, tambien le Local storage para que no se borren las tareas

--- js/script.js
+++ js/script.js
@@ -225,3 +225,134 @@ document.addEventListener("DOMContentLoaded", function () {
   // Inicializar los selects de unidad con la primera categoría por defecto
   populateUnitSelect(categorySelect.value);
 });
+
+/* Modo oscuro */
+/*
+function modoOscuro() {
+  const body = document.body;
+  const btn =
+    document.querySelector(".nav-btn") ||
+    document.querySelector(".nav-btn-claro");
+  const elementsToToggle = document.querySelectorAll(
+    ".modo-oscuro, .modo-claro"
+  );
+
+  if (body.classList.contains("modo-claro")) {
+    body.classList.remove("modo-claro");
+    body.classList.add("modo-oscuro");
+
+    elementsToToggle.forEach((element) => {
+      element.classList.remove("modo-claro");
+      element.classList.add("modo-oscuro");
+    });
+
+    if (btn) {
+      btn.classList.remove("nav-btn");
+      btn.classList.add("nav-btn-claro");
+    }
+  } else {
+    body.classList.remove("modo-oscuro");
+    body.classList.add("modo-claro");
+
+    elementsToToggle.forEach((element) => {
+      element.classList.remove("modo-oscuro");
+      element.classList.add("modo-claro");
+    });
+
+    if (btn) {
+      btn.classList.remove("nav-btn-claro");
+      btn.classList.add("nav-btn");
+    }
+  }
+}
+*/
+
+// Recuperar tareas almacenadas al cargar la página
+document.addEventListener("DOMContentLoaded", function () {
+  cargarTareas();
+});
+
+function agregarTarea() {
+  // Obtener el valor del campo de la tarea
+  const nuevaTareaTexto = document.querySelector(".display-tareas").value;
+
+  // Validar que el valor no sea vacío
+  if (nuevaTareaTexto === "") {
+    alert("Por favor, ingrese una tarea");
+    return;
+  }
+
+  // Crear un elemento en la lista
+  const nuevaTarea = document.createElement("li");
+  nuevaTarea.textContent = nuevaTareaTexto + " ";
+
+  // Crear botón eliminar
+  const botonEliminar = document.createElement("button");
+  botonEliminar.textContent = "X";
+  botonEliminar.onclick = function () {
+    nuevaTarea.remove();
+    // Eliminar tarea de Local Storage
+    eliminarTareaLocalStorage(nuevaTareaTexto);
+  };
+
+  // Agregar botón de eliminar al elemento de la lista
+  nuevaTarea.appendChild(botonEliminar);
+
+  // Agregar los elementos en la lista final
+  document.getElementById("listaTareas").appendChild(nuevaTarea);
+
+  // Guardar tarea en Local Storage
+  guardarTareaLocalStorage(nuevaTareaTexto);
+
+  // Limpiar el cuadro de texto del nombre de la tarea
+  document.querySelector(".display-tareas").value = "";
+}
+
+function guardarTareaLocalStorage(tarea) {
+  let tareas = obtenerTareasLocalStorage();
+  tareas.push(tarea);
+  localStorage.setItem("tareas", JSON.stringify(tareas));
+}
+
+function obtenerTareasLocalStorage() {
+  let tareas;
+  if (localStorage.getItem("tareas") === null) {
+    tareas = [];
+  } else {
+    tareas = JSON.parse(localStorage.getItem("tareas"));
+  }
+  return tareas;
+}
+
+function cargarTareas() {
+  let tareas = obtenerTareasLocalStorage();
+  tareas.forEach(function (tarea) {
+    // Crear un elemento en la lista
+    const nuevaTarea = document.createElement("li");
+    nuevaTarea.textContent = tarea + " ";
+
+    // Crear botón eliminar
+    const botonEliminar = document.createElement("button");
+    botonEliminar.classList.add("boton-eliminar");
+    botonEliminar.textContent = "X";
+    botonEliminar.onclick = function () {
+      nuevaTarea.remove();
+      // Eliminar tarea de Local Storage
+      eliminarTareaLocalStorage(tarea);
+    };
+
+    // Agregar botón de eliminar al elemento de la lista
+    nuevaTarea.appendChild(botonEliminar);
+
+    // Agregar los elementos en la lista final
+    document.getElementById("listaTareas").appendChild(nuevaTarea);
+  });
+}
+
+function eliminarTareaLocalStorage(tarea) {
+  let tareas = obtenerTareasLocalStorage();
+  tareas = tareas.filter(function (t) {
+    return t !== tarea;
+  });
+  localStorage.setItem("tareas", JSON.stringify(tareas));
+}
